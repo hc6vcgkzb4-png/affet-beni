ed47ebf669353eb92e227ca4c5ee9f0fdeabd5b6
hc6vcgkzb4-png <hc6vcgkzb4@privaterelay.appleid.com>
2026-01-30 18:40:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+function goYes() {
+  window.location.href = "yes.html";
+}
